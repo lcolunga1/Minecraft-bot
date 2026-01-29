a11d968b8218cf02820f01e8225df0ccad7213e2
lcolunga1 <luis255357@gmail.com>
2026-01-29 15:44:02 -0600

    Crear index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import { startHttpServer } from "./src/http.js";
+import { createLogger } from "./src/logger.js";
+import { createReputationStore } from "./src/reputation.js";
+import { createBrain } from "./src/ai.js";
+import { createMinecraftBot } from "./src/mc.js";
+
+startHttpServer();
+
+const logger = createLogger();
+const reputation = createReputationStore();
+const brain = createBrain({ logger });
+
+createMinecraftBot({ logger, brain, reputation });
+
+console.log(`[BOOT] logs -> ${logger.file}`);
